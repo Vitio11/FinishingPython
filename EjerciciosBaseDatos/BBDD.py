cdcc3ff00aad73e2f6ab730ff44a53f4905d60e9
Vitio11 <vmperezrosa@gmail.com>
2023-05-03 01:12:03 +0200

    actualizacion

--- EjerciciosBaseDatos/BBDD.py
+++ EjerciciosBaseDatos/BBDD.py
@@ -1,6 +1,9 @@
 from pymongo.mongo_client import MongoClient
+import random
 import json
 import os
+
+
 def connectdb():
     clientDB = MongoClient('mongodb://localhost:27017/')
     db = clientDB.admin
@@ -13,9 +16,70 @@ def connectdb():
 
 #Create a database
 def CreateDB(name,clientDb):
-    database_name=name #'Students'
+    database_name=name 
     db=clientDb[database_name]
     return db
+
+
+#Create a collection
+def createCollection(name,db):
+    collection_name=name #'computer science'
+    collection=db[collection_name]
+    return collection
+
+
+def ListaHM():
+        ListaHM=[]
+        for x in range(1,101):
+            generarGenero= random.randint(0,1)
+            if generarGenero==0:
+                ListaHM.append("H")
+            else:
+                ListaHM.append("M")
+        #try:
+        #    col.insert_one(ListaHM) #inserta datos en BD
+       # except Exception as e: print(e)
+        f=open("ListaHM", "wt", encoding='UTF-8')
+        f.write(str(ListaHM)) #crea JSON
+        f.close()
+        return ListaHM
+
+
+def retrieveAllDocuments(col):
+ result=col.find({}).limit(200)
+ for i in result:
+        print(i)
+#filtering
+
+if __name__ == '__main__':
+    client=None
+    db=None
+    col=None
+    while 1:
+        os.system("clear")
+        print('1 - Conectar con Mongo')
+        print('2 - Crear una  Base de datos')
+        print('3 - Crear una colección')
+        print('4 - Crear una lista aleatoria de 100 hombres y mujeres y guardarla')
+        print('5 - Mostrar base de datos')
+
+        print('0 - Salir')
+        selection=input('Introduce el numero de la accion a realizar: ')
+        if selection == '1':
+            client=connectdb()
+        elif selection =='2':    
+            db=CreateDB("HombreYMujeres",client)    
+        elif selection =='3': 
+            col=createCollection('ListaHyM',db)  
+        elif selection =='4': 
+            lista = ListaHM()
+        elif selection =='5': 
+            retrieveAllDocuments(col)
+        elif selection =='0':     
+            break                                                               
+        else:
+            print('Invalid selection, try again')
+        follow=input("Pres any key to continue...")
 
 
 
@@ -37,9 +101,9 @@ def GenerarListaGenero(self): #para crear la lista random de hombres y mujeres
         for x in range(1,101):
             generarGenero= random.randint(0,1)
             if generarGenero==0:
-                self.listaElementos.append("H")
+                self.ListaHM.append("H")
             else:
-                self.listaElementos.append("M")
+                self.ListaHM.append("M")
         print('Lista creada satisfactoriamente.')
         print('Creando fichero...')
         self.CreacionFichero()
