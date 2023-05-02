9b89d0986b23fa4731bc84bd9f49f23bab12fc92
miguelbemon <miguelbenitezmontesinosdev@gmail.com>
2023-05-02 20:21:27 +0100

    IncioFlask

---
+++ EjerciciosFlask/app.py
@@ -0,0 +1,12 @@
+#Mostrar una página que ponga 6 opciones: (1)
+
+#Elejir para las 6 opciones 6 de los 20 ejercicios (DNI, Salario, Par o Impar, Celsius, etc)
+
+#Cuando se pase a la página de la opción, se verá su funcionalidad
+
+#Total de paginas HTML dinámicas: 12
+#Pagina de solicitud de datos
+#Pagina de resultados
+
+
+
