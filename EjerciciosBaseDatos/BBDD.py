899ac1ccfd0450c1415b8abaf4410dcbbafdd4fa
Vitio11 <vmperezrosa@gmail.com>
2023-05-03 10:02:42 +0200

    ejercicios bases de datos

--- EjerciciosBaseDatos/BBDD.py
+++ EjerciciosBaseDatos/BBDD.py
@@ -23,38 +23,52 @@ def CreateDB(name,clientDb):
 
 #Create a collection
 def createCollection(name,db):
-    collection_name=name #'computer science'
+    collection_name=name 
     collection=db[collection_name]
+    col=collection
     return collection
 
 
-def ListaHM():
+def ListaHM(col):
         ListaHM=[]
-        for x in range(1,101):
-            generarGenero= random.randint(0,1)
-            if generarGenero==0:
-                ListaHM.append("H")
-            else:
-                ListaHM.append("M")
-        #try:
-        #    col.insert_one(ListaHM) #inserta datos en BD
-       # except Exception as e: print(e)
+        for i in range(100):
+            genero = random.choice(["H", "M"])
+            ListaHM.append(genero)
+            col.insert_one({"genero": genero}) # Insertar en MongoDB
         f=open("ListaHM", "wt", encoding='UTF-8')
-        f.write(str(ListaHM)) #crea JSON
+        f.write(str(ListaHM)) #crea JSON en mi carpeta local
         f.close()
         return ListaHM
 
 
-def retrieveAllDocuments(col):
- result=col.find({}).limit(200)
- for i in result:
-        print(i)
-#filtering
+def retrieveAllDocuments(col): #muestra lo introducido
+    try:
+        result=col.find({}).limit(200)
+        for i in result:
+            print(i)
+    except:
+        print("Para mostrar la base de datos primero tienes que conectar con Mongo y crearla con los pasos 1,2,3 y 4")
+
+def EstadisticasHyM():
+    ListaHM=open("ListaHM",'rt', encoding='UTF-8')
+    Lista=ListaHM.read()
+    nH= Lista.count("H")
+    nM=Lista.count("M")
+    Estadisticas["Numero Hombres"]=nH
+    Estadisticas["Numero Mujeres"]=nM
+    Estadisticas["PorcentajeHombres"]= nH
+    Estadisticas["PorcentajeMuejeres"]= nM
+    for clave in Estadisticas:
+        col.insert_one({clave:Estadisticas[clave]})
+
+    print(Estadisticas)
+    
 
 if __name__ == '__main__':
     client=None
     db=None
     col=None
+    Estadisticas={}
     while 1:
         os.system("clear")
         print('1 - Conectar con Mongo')
@@ -62,6 +76,7 @@ if __name__ == '__main__':
         print('3 - Crear una colección')
         print('4 - Crear una lista aleatoria de 100 hombres y mujeres y guardarla')
         print('5 - Mostrar base de datos')
+        print('6 - Mostrar Estadisticas de las bases de datos')
 
         print('0 - Salir')
         selection=input('Introduce el numero de la accion a realizar: ')
@@ -72,11 +87,13 @@ if __name__ == '__main__':
         elif selection =='3': 
             col=createCollection('ListaHyM',db)  
         elif selection =='4': 
-            lista = ListaHM()
+            listaHM = ListaHM(col)
         elif selection =='5': 
             retrieveAllDocuments(col)
+        elif selection =='6': 
+            EstadisticasHyM()
         elif selection =='0':     
             break                                                               
         else:
-            print('Invalid selection, try again')
-        follow=input("Pres any key to continue...")
+            print('Seleccion invalida, introduce una opcion dentro de la lista')
+        follow=input("Presiona cualquier tecla para continuar")
